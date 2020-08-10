5d13f47d2cdecd605135b8d016153d474bdbddb9
avkgeo <avakya.georgiy00@gmail.com>
2020-08-10 11:10:11 +0300

    123

---
+++ app/src/main/java/com/huskyjam/motion/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.huskyjam.motion
+
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import androidx.constraintlayout.motion.widget.MotionLayout
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity)
+
+    }
+
+}
